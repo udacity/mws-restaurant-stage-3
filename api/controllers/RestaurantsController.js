283578d434cfa3ea0b26394c11fa640c018781b4
David Harris <david@david.harris@udacity.com>
2017-10-13 10:45:07 -0700

    replace with complete server files - some files were missing

---
+++ api/controllers/RestaurantsController.js
@@ -0,0 +1,8 @@
+/**
+ * RestaurantsController
+ *
+ * @description :: Server-side logic for managing restaurants
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
---
+++ api/controllers/ReviewsController.js
@@ -0,0 +1,8 @@
+/**
+ * ReviewsController
+ *
+ * @description :: Server-side logic for managing reviews
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
